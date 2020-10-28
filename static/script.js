16e8dbc4e3a0d35baf17796f7813e2ec5c91f8f3
saadati944 <saadati944@gmail.com>
2020-10-28 12:59:08 +0330

    handle image command in web page

--- static/script.js
+++ static/script.js
@@ -1,16 +1,33 @@
+var id=0;
+
 // download commands from server...
 function getcommand()
 {
     fetch("http://localhost:5000/getcommand").then(function(response){
         response.json().then(function(command){
+            id+=1;
             if(command['cmd']=='hello')
             {
                 alert('hello');
             }
+            //handle image command
             else if (command['cmd']=="image")
             {
-                
+                let img=document.createElement('img');
+                img.setAttribute('src',command['src']);
+                img.setAttribute('style',"width:"+command['width']+"px; height:"+command['height']+"px; top:"+command['y']+"px; left:"+command['x']+"px;")
+                let removeid='delete_this_item_with_id:'+id.toString();
+                img.setAttribute("id",removeid);
+
+                document.getElementById("body").appendChild(img);
+
+                if (command['duration']!=-1)
+                {
+                    setTimeout("document.getElementById('"+removeid+"').remove()",command['duration']);
+                }
+                getcommand();
             }
+            
         })
     });
 }
